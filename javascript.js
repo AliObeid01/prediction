eb7154bf53a016b0e9c0203568d0afa3f5a89664
unknown <aliiobeid01@gmail.com>
2022-09-11 06:29:33 +0300

    added eventlistner to the button,get the user input,handle the user input and fetch the api of the gender

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ const country=document.getElementById("country");
 const predict=document.getElementById("predict");
 const error_message=document.getElementById("empty");
 
+predict.addEventListener('click', getdata);
 
 window.onload=(loaded) =>{
 
@@ -18,3 +19,30 @@ window.onload=(loaded) =>{
    
 }
 
+function getdata(){
+
+    const user_input=document.getElementById("name");
+    if(user_input.value.length == 0){
+      error_message.innerHTML=`Please enter a name!`
+    }
+    else{
+      error_message.innerHTML=``
+      request(user_input);
+    }
+    
+
+}
+
+request=(user_input) =>{
+
+   let url_gender=`https://api.genderize.io?name=${user_input.value}`;
+   fetch(url_gender)
+   .then(result => result.json())
+   .then(data => {
+      gender.innerHTML=`${data.gender}`
+
+   })
+
+
+
+}
